caf8955806a9711c5c3187f3b077bb4b93aeee99
Vietson Nguyen <vietson@gmail.com>
2012-10-29 17:00:08 -0400

    worked on admin category pages

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -5,6 +5,8 @@ class Admin::CategoriesController < Admin::ApplicationController
   end
   
   def show
+    @category = Category.find(params[:id])
+    @post = @category.posts
   end
   
   def new
@@ -15,7 +17,7 @@ class Admin::CategoriesController < Admin::ApplicationController
     @category = Category.new(params[:category])
     respond_to do |format|
       if @category.save
-        flash[:success] = "Category Created"
+        flash[:success] = "Category Created!"
         format.html { redirect_to admin_categories_path }
       else
         flash[:error] = "Error saving category!"
@@ -23,4 +25,29 @@ class Admin::CategoriesController < Admin::ApplicationController
       end
     end
   end
+  
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update
+    @category = Category.find(params[:id])
+    respond_to do |format|
+      if @category.update_attributes(params[:category])
+        flash[:success] = "Category updated!"
+        format.html { redirect_to admin_categories_path }
+      else
+        flash[:error] = "Error updating category"
+      end
+    end
+  end
+  
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_categories_path }
+    end
+  end
+  
 end
