1ebfb35392ce50acb841583e49787cbd23eb70c3
Vietson Nguyen <vietson@gmail.com>
2012-11-09 10:18:34 -0500

    rspec controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,31 +1,13 @@
-class CategoriesController < ApplicationController
-  before_filter :authenticate_user!, :only => [:edit, :update, :destoy]
-  before_filter :get_cat_id, :only => [:update, :desroy, :edit]
-  
+class CategoriesController < ApplicationController  
   def index
     @categories = Category.all
   end
   
   def show
-    @category = Category.find(:all, :include => :posts)
-  end
-  
-  def create
-    @category = Category.new(params[:category])
-  end
-  
-  def edit
-  end
-  
-  def update
-  end
-  
-  def destroy 
-    @category.destroy
-    redirect_to root_path, :notice => 'Successfully removed post!'
-  end
-  
-  def get_cat_id
     @category = Category.find(params[:id])
+  end
+  
+  def new
+    
   end
 end
